45d49315e9e861f90858ba572a21e9d3f1e111b3
MorimotoYura <241103@std.hi-joho.ac.jp>
2025-05-21 10:10:42 +0900

    Aクラスを追加 コメント文を追加 復習問題2をすべてやった

--- src/Review25.java
+++ src/Review25.java
@@ -1,4 +1,10 @@
 public class Review25 {
     public static void main(String[] args) {
+        int[] numArray = {11,12,13,14,15};
+        int sum = 0;
+        for (int i = 0; i < numArray.length; i++) {
+            sum += numArray[i];
+        }
+        System.out.println("結果" + sum);
     }
 }
