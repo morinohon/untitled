a6f8d80b8f4e245a6919440489ba71bba10b913d
MorimotoYura <241103@std.hi-joho.ac.jp>
2025-05-26 12:27:25 +0900

    練習問題1を9までやった。

---
+++ src/Rensyumondai1/Review4.java
@@ -0,0 +1,16 @@
+package Rensyumondai1;
+
+public class Review4 {
+    public static void main(String[] args) {
+        int number = 5;
+        String str = 88 + "88";
+        final double PI = 3.141592;
+        double pi = 3.14;
+        float circ = 3.14F;
+        long a = 10000;
+        boolean fig = true;
+        byte b = 35;
+        int num  = 0x735;
+        char c = 'a';
+    }
+}
---
+++ src/Rensyumondai1/Review8.java
@@ -0,0 +1,14 @@
+package Rensyumondai1;
+
+public class Review8 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a;
+        a = (int)b;
+        a = (int)10000L;
+        b = 10000L;
+        b = (int)a;
+        float f = (float)5.94;
+        short s = 10;
+    }
+}
