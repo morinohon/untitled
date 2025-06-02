bfb5c6a224ab1d058f71470786a60dfd5d24fc60
MorimotoYura <241103@std.hi-joho.ac.jp>
2025-06-02 12:27:16 +0900

    練習問題3を終わらせた

---
+++ src/Rensyumondai3/Review29/KeyInput.java
@@ -0,0 +1,16 @@
+package Rensyumondai3.Review29;
+
+import java.io.*;
+
+public class KeyInput {
+    public String readString() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputStr = "";
+        try{
+            inputStr = br.readLine();
+        }catch(IOException e){
+            System.out.println("ERROR: IOException");
+        }
+        return inputStr;
+    }
+}
